bb064b97c0aa57eac3400bc3cbe464120b1a882d
Lev <Lev-person11@yandex.ru>
2025-08-06 17:54:44 +0300

    adding fixing get chat error

--- src/error.ts
+++ src/error.ts
@@ -3,7 +3,6 @@ import { ChatFullInfo } from "grammy/types"
 
 export function getChatError(id: number): (err: any) => ChatFullInfo {
     return err => {
-        console.error(err)
         if (err instanceof GrammyError && err.description === 'Bad Request: chat not found')
             return {
                 id,
